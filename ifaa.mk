5bea6b6afd66776d52a876e3b4aec5e726539eec
nekojackie <jackieawa@outlook.com>
2024-11-20 23:23:39 +0800

    Add namespace

--- ifaa.mk
+++ ifaa.mk
@@ -1,5 +1,7 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/IfaaService
+    vendor/motorola/sm8475-common \
+    vendor/motorola/eqs \
 
 PRODUCT_COPY_FILES += \
     vendor/motorola/IfaaService/proprietary/vendor/etc/init/vendor.zui.hardware.ifaa@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.zui.hardware.ifaa@1.0-service.rc \
@@ -7,5 +9,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     IfaaService \
-    vendor.zui.hardware.ifaa@1.0.so \
-    vendor.zui.hardware.ifaa@1.0.so \
+    vendor.zui.hardware.ifaa@1.0-service \
+    vendor.zui.hardware.ifaa@1.0 \
+

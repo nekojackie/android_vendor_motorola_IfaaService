4294e539acc42f1af02654524d4efcc7f4466cf3
NekoJackie <jackieawa@outlook.com>
2025-03-31 01:12:08 +0800

    Complete the librarie which IfaaService need

--- ifaa.mk
+++ ifaa.mk
@@ -11,9 +11,44 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/motorola/tcmd/ifaa.pubkey:$(TARGET_COPY_OUT_VENDOR)/etc/motorola/tcmd/ifaa.pubkey
 
 PRODUCT_PACKAGES += \
+    libDxHdcp \
+    libagm \
+    libagmclient \
+    libagmmixer \
+    libar-acdb \
+    libar-gpr \
+    libar-gsl \
+    libats \
+    libaudio_log_utils \
+    libhdcp1prov \
+    libhdcp2p2prov \
+    liblx-ar_util \
+    liblx-osal \
+    liboemcrypto \
+    libpalclient \
+    libpdmapper \
+    libpdnotifier \
+    libqmi_cci \
+    libqmi_common_so \
+    libqmi_encdec \
+    libstfactory-vendor \
+    libtcmd_diag \
+    libtpa \
+    libQSEEComAPI \
     lib_ifaa_ca \
     lib_ifaa_product \
+    libcpion \
+    libdiag \
+    liblog_vendor \
+    libminkdescriptor \
+    libminksocket_vendor \
     libpkip \
+    libqcbor \
+    libqrtr \
+    libsotertool \
+    libtrustedapploader \
+    motorola.hardware.tcmd@1.0 \
+    vendor.qti.hardware.soter@1.0 \
     vendor.zui.hardware.ifaa@1.0 \
     IfaaService \
     vendor.zui.hardware.ifaa@1.0-service \
